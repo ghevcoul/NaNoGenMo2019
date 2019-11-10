098f329df23b14783219090f22ac67a21590d3e9
Gavin Heverly-Coulson <gavin@quantumgeranium.com>
2019-11-10 14:08:31 -0800

    convert old fractal tree code to a class

--- FractalTree/fractal_tree.py
+++ FractalTree/fractal_tree.py
@@ -1,123 +1,153 @@
 """
 A recursive fractal tree builder.
 """
-import sys
 import math
 import random
 import time
 from multiprocessing import Queue, JoinableQueue, Process
+from dataclasses import dataclass
+
+from typing import List, Tuple
 
 
-PARAMS = {
-    "length": (0.45, 0.85),
-    "angle": (-65, 65),
-    "branches": [2, 2, 2, 3, 3, 3, 4, 4, 5]
-}
-
-def make_branch(start, length, angle):
+@dataclass
+class Point:
     """
-    Gets the x,y coordinates for the end of a branch with the input starting coordinates
-    with branch length and angle.
-    Returns a tuple of (x1, y1, x2, y2)
+    Cartesian point
     """
-    x_end = start[0] + (length * math.cos(math.radians(angle)))
-    y_end = start[1] + (length * math.sin(math.radians(angle)))
-    return (start[0], start[1], x_end, y_end)
+    x: float
+    y: float
 
-def worker(work_q, res_q):
 
-    while not work_q.empty():
-        task = work_q.get()
-        start, length, angle = task
-        if length > 3:
-            # Generate this branch
-            branch = make_branch(start, length, angle)
-            res_q.put(branch)
-            # Put the next branches on the work queue
-            next_pt = branch[2:]
-            for _ in range(random.choice(PARAMS["branches"])):
-                work_q.put(
-                    (
-                        next_pt,
-                        length * random.uniform(PARAMS["length"][0], PARAMS["length"][1]),
-                        angle + random.randrange(PARAMS["angle"][0], PARAMS["angle"][1])
-                    )
+@dataclass
+class LineSegment:
+    """
+    Line segment defined by two pairs of Cartesian xy coordinates
+    """
+    start: Point
+    end: Point
+
+    def length(self) -> float:
+        """
+        Calculates the length of the LineSegment
+        """
+        return math.sqrt((self.end.x - self.start.x) ** 2 - (self.end.y - self.start.y) ** 2)
+
+
+class FractalTree:
+    """
+    A class for building randomized fractal trees.
+    """
+    def __init__(self):
+        random.seed(36797)
+
+        self.trunk_length: int = 150
+        self.start_angle: int = 270
+        self.start_pos: Point = Point(0, 0)
+
+        # Will the tree be symmetrical? 65% chance of asymmetry
+        #TODO: Implement symmetrical trees
+        self.symmetrical: bool = random.random() > 0.65
+        # What fraction will the length of the next branch be reduced
+        self.length: Tuple[float, float] = (0.45, 0.85)
+        # What angle (in degrees) will the next branch have
+        self.angle: Tuple[int, int] = (-65, 65)
+        # How many branches will come off this branch
+        self.branches: List[int] = [2, 2, 2, 3, 3, 3, 4, 4, 5]
+
+        self.tree: List[LineSegment] = []
+
+    def generate(self):
+        """
+        Generate a tree using the parameters set in the constructor.
+        Calls a recursive function that actually does the tree building with the starting values
+        """
+        self._build_tree(self.start_pos, self.trunk_length, self.start_angle)
+
+    def _build_tree(self, start: Point, branch_len: float, branch_angle: float):
+        """
+        Recursively build the tree, exiting when the branch length is 3 or less.
+        """
+        if branch_len > 3:
+            branch = self._make_branch(start, branch_len, branch_angle)
+            self.tree.append(branch)
+
+            for _ in range(random.choice(self.branches)):
+                self._build_tree(
+                    branch.end,
+                    branch_len * random.uniform(self.length[0], self.length[1]),
+                    branch_angle + random.randrange(self.angle[0], self.angle[1])
                 )
-        work_q.task_done()
 
-def build_tree_parallel(start=(0, 0), branch_len=150, angle=270):
-
-    # Make a work queue and result queue
-    work_queue = JoinableQueue()
-    result_queue = Queue()
-
-    # Put the first task in the work queue
-    work_queue.put([start, branch_len, angle])
-
-    # print(work_queue.get())
-    # sys.exit()
-
-    # Start a bunch of workers
-    workers = 2
-    processes = []
-    for _ in range(workers):
-        p = Process(target=worker, args=(work_queue, result_queue))
-        p.start()
-        processes.append(p)
-        time.sleep(0.25)
-    work_queue.join()
-    # for p in processes:
-    #     p.join()
-    work_queue.close()
-
-    # Collect the results...
-    tree = []
-    while not result_queue.empty():
-        tree.append(result_queue.get())
-    result_queue.close()
-    print(len(tree))
-    return tree
+    @staticmethod
+    def _make_branch(start: Point, branch_length: float, branch_angle: float) -> LineSegment:
+        """
+        Gets the xy coordinates for the end of a branch with the input starting coordinates
+        with branch length and angle.
+        Returns a LineSegment
+        """
+        end_point = Point()
+        end_point.x = start.x + (branch_length * math.cos(math.radians(branch_angle)))
+        end_point.y = start.y + (branch_length * math.sin(math.radians(branch_angle)))
+        return LineSegment(start, end_point)
 
 
-def build_tree(start=(0, 0), branch_len=150, angle=270):
-    """
-    A recursive function to build a fractal tree.
-
-    Input:
-    start - (x, y) tuple giving the starting point of the tree
-    branch_len - the length of this branch of the tree
-
-    Output:
-    tree - list of (x1, y1, x2, y2) defining the line segments of this tree
-    """
-    params = {
-        "length": (0.45, 0.825),
-        "angle": (-65, 65),
-        "branches": [2, 2, 2, 3, 3, 3, 4, 5]
-    }
-
-    if branch_len <= 3:
-        return []
-    else:
-        tree = []
-        branch = make_branch(start, branch_len, angle)
-        tree.append(branch)
-
-        for _ in range(random.choice(params["branches"])):
-            tree += build_tree(
-                (branch[2], branch[3]),
-                branch_len * random.uniform(
-                    params["length"][0],
-                    params["length"][1]
-                ),
-                angle + random.randrange(params["angle"][0], params["angle"][1])
-            )
-
-        return tree
-
+# def worker(work_q, res_q):
+#
+#     while not work_q.empty():
+#         task = work_q.get()
+#         start, length, angle = task
+#         if length > 3:
+#             # Generate this branch
+#             branch = make_branch(start, length, angle)
+#             res_q.put(branch)
+#             # Put the next branches on the work queue
+#             next_pt = branch[2:]
+#             for _ in range(random.choice(PARAMS["branches"])):
+#                 work_q.put(
+#                     (
+#                         next_pt,
+#                         length * random.uniform(PARAMS["length"][0], PARAMS["length"][1]),
+#                         angle + random.randrange(PARAMS["angle"][0], PARAMS["angle"][1])
+#                     )
+#                 )
+#         work_q.task_done()
+#
+# def build_tree_parallel(start=(0, 0), branch_len=150, angle=270):
+#
+#     # Make a work queue and result queue
+#     work_queue = JoinableQueue()
+#     result_queue = Queue()
+#
+#     # Put the first task in the work queue
+#     work_queue.put([start, branch_len, angle])
+#
+#     # print(work_queue.get())
+#     # sys.exit()
+#
+#     # Start a bunch of workers
+#     workers = 2
+#     processes = []
+#     for _ in range(workers):
+#         p = Process(target=worker, args=(work_queue, result_queue))
+#         p.start()
+#         processes.append(p)
+#         time.sleep(0.25)
+#     work_queue.join()
+#     # for p in processes:
+#     #     p.join()
+#     work_queue.close()
+#
+#     # Collect the results...
+#     tree = []
+#     while not result_queue.empty():
+#         tree.append(result_queue.get())
+#     result_queue.close()
+#     print(len(tree))
+#     return tree
 
 
 if __name__ == "__main__":
     # random.seed(1234)
-    TREE = build_tree_parallel()
-
+    TREE = FractalTree()
+    TREE.generate()
